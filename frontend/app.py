65b51f2788f298a47305f566f17371d34d17e375
abhimanyu <abhimanyu@abhimanyus-MacBook-Air.local>
2025-12-06 01:04:07 +0530

    update k8s deployment file

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    # Read API base URL from environment variables
+    api_base = os.getenv("API_BASE", "http://localhost:8000")
+    return render_template("index.html", api_base=api_base)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
